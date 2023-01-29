67afdf53e2e77c36a260f17d003befc8b7d1eb06
ambiso <ambisotopy@gmail.com>
2023-01-29 17:18:15 +0100

    Initial

---
+++ RainWorldSavesManager.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using System.Security.Permissions;
+
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+namespace RainWorldSavesManager
+{
+	[BepInPlugin("ambiso.rwsm", "RainWorldSavesManager", "0.1.0")]
+	public class RainWorldSavesManager : BaseUnityPlugin
+	{
+		public void OnEnable()
+		{
+			/* This is called when the mod is loaded. */
+			// subscribe your PlayerUpdateHook to the Player.Update method from the game
+			Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA Hooking");
+			On.RainWorld.OnModsInit += OnModsInitHook;
+		}
+
+		void OnModsInitHook(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+		{
+			Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+			orig(self);
+		}
+	}
+}
